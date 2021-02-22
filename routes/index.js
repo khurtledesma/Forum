0fd10017b7a6e4b8bfdf079f25d49852c296aa84
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-02-22 17:16:26 -0600

    updated to show all posts

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuthenticated} = require("../config/auth.js");
+const { ensureAuthenticated } = require("../config/auth.js");
 const Post = require('../models/post');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Welcome', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('login');
 });
 
-router.get('/register', (req,res)=>{
+router.get('/register', (req, res) => {
   res.render('register');
 })
 
-router.get('/dashboard', ensureAuthenticated, (req,res)=>{
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
   Post.find()
-  
-  .then((result) => {
-    res.render('dashboard', {
-    posts: result,
-    user: req.user
-  })
-  })
+    .populate({ path: 'submittedBy', select: 'name' })
+    .then((result) => {
+      console.log()
+      res.render('dashboard', {
+        posts: result,
+        user: req.user,
+      })
+      
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
 module.exports = router;
